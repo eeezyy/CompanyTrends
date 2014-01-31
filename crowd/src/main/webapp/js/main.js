1269e9e10e4eaba06d1066c12f1d0dc851125274
Alexander Kumbeiz <eeezyy@gmail.com>
2014-01-31 03:24:22 +0100

    rating and rating color indicator in joblist; refactored color function

--- crowd/src/main/webapp/js/main.js
+++ crowd/src/main/webapp/js/main.js
@@ -27,8 +27,53 @@ app.factory('CommonService', function() {
 		return dateText;
 	};
 	
+	var formatTwoDecimalsAndRound = function(value) {
+		return Math.round(value * 100)/100;
+	};
+	
+	// code from: http://jsfiddle.net/rE6Rk/1/
+	// convert a number to a color using hsl
+	function numberToColorHsl(i) {
+		// adjust rating to range from 0 to 100
+		i = (i + 1) * 50;
+	    // as the function expects a value between 0 and 1, and red = 0° and green = 120°
+	    // we convert the input to the appropriate hue value
+	    var hue = i * 1.2 / 360;
+	    // we convert hsl to rgb (saturation 100%, lightness 50%)
+	    var rgb = hslToRgb(hue, 1, .5);
+	    // we format to css value and return
+	    return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'; 
+	}
+	
+	function hslToRgb(h, s, l){
+	    var r, g, b;
+
+	    if(s == 0){
+	        r = g = b = l; // achromatic
+	    }else{
+	        function hue2rgb(p, q, t){
+	            if(t < 0) t += 1;
+	            if(t > 1) t -= 1;
+	            if(t < 1/6) return p + (q - p) * 6 * t;
+	            if(t < 1/2) return q;
+	            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+	            return p;
+	        }
+
+	        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+	        var p = 2 * l - q;
+	        r = hue2rgb(p, q, h + 1/3);
+	        g = hue2rgb(p, q, h);
+	        b = hue2rgb(p, q, h - 1/3);
+	    }
+
+	    return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
+	}
+	
 	return {
 		formatDate: formatDate,
+		formatTwoDecimalsAndRound: formatTwoDecimalsAndRound,
+		numberToColorHsl: numberToColorHsl,
 	};
 });
 
@@ -162,6 +207,8 @@ function SearchCtrl(CrowdRestangular, DBPediaRestangular, CommonService, $scope,
 function JobCtrl(CrowdRestangular, DBPediaRestangular, CommonService, $scope, $http, $routeParams, $route) {
 	// init common functions
 	$scope.formatDate = CommonService.formatDate;
+	$scope.formatTwoDecimalsAndRound = CommonService.formatTwoDecimalsAndRound;
+	$scope.numberToColorHsl = CommonService.numberToColorHsl;
 	
 	$scope.showLoading = function() {
 		return $scope.loading;
@@ -221,57 +268,17 @@ function JobCtrl(CrowdRestangular, DBPediaRestangular, CommonService, $scope, $h
 		return Math.round(value * 100);
 	};
 	
-	$scope.formatTwoDecimalsAndRound = function(value) {
-		return Math.round(value * 100)/100;
-	};
-	
 	$scope.trendColor = function(ratingResult) {
 		
 	};
 	
-	// code from: http://jsfiddle.net/rE6Rk/1/
-	// convert a number to a color using hsl
-	$scope.numberToColorHsl = function(i) {
-		// adjust rating to range from 0 to 100
-		i = (i + 1) * 50;
-	    // as the function expects a value between 0 and 1, and red = 0° and green = 120°
-	    // we convert the input to the appropriate hue value
-	    var hue = i * 1.2 / 360;
-	    // we convert hsl to rgb (saturation 100%, lightness 50%)
-	    var rgb = hslToRgb(hue, 1, .5);
-	    // we format to css value and return
-	    return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'; 
-	}
-	
-	function hslToRgb(h, s, l){
-	    var r, g, b;
-
-	    if(s == 0){
-	        r = g = b = l; // achromatic
-	    }else{
-	        function hue2rgb(p, q, t){
-	            if(t < 0) t += 1;
-	            if(t > 1) t -= 1;
-	            if(t < 1/6) return p + (q - p) * 6 * t;
-	            if(t < 1/2) return q;
-	            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-	            return p;
-	        }
-
-	        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-	        var p = 2 * l - q;
-	        r = hue2rgb(p, q, h + 1/3);
-	        g = hue2rgb(p, q, h);
-	        b = hue2rgb(p, q, h - 1/3);
-	    }
-
-	    return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
-	}
 }
 
 function JobListCtrl(CrowdRestangular, CommonService, $scope) {
 	// init common functions
 	$scope.formatDate = CommonService.formatDate;
+	$scope.formatTwoDecimalsAndRound = CommonService.formatTwoDecimalsAndRound;
+	$scope.numberToColorHsl = CommonService.numberToColorHsl;
 
 	$scope.showLoading = function() {
 		return $scope.loading;
